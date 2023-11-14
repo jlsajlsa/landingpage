3bdf0d0553d9496802954ae44f0efa32c29d9386
managerweknow <admin@we-know.net>
2023-11-14 10:16:16 -0600

    Ajustar la galería de imagenes para poder poner en un modal el efecto de anterior o siguiente

--- assets/js/generla.js
+++ assets/js/generla.js
@@ -5,3 +5,45 @@ document.addEventListener("DOMContentLoaded", function() {
     // Click al boton del menu collapsable
     clickMainTitle();
 });
+
+let slideIndex = 0;
+
+function openModal(imageSrc) {
+    const modal = document.getElementById('myModal');
+    const modalImg = document.getElementById('modalImage');
+
+    modal.style.display = 'flex';
+    modalImg.src = imageSrc;
+    slideIndex = getCurrentIndex(imageSrc);
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    const modalImg = document.getElementById('modalImage');
+    const images = document.querySelectorAll('.gallery img');
+
+    if (n >= images.length) {
+        slideIndex = 0;
+    } else if (n < 0) {
+        slideIndex = images.length - 1;
+    }
+
+    modalImg.src = images[slideIndex].src;
+}
+
+function getCurrentIndex(imageSrc) {
+    const images = document.querySelectorAll('.gallery img');
+    for (let i = 0; i < images.length; i++) {
+        if (images[i].src === imageSrc) {
+            return i;
+        }
+    }
+    return 0;
+}
